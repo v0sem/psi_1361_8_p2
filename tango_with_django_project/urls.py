198e4c934729108e5f3d5971f5002e7bb0e03f8e
vosem <progpabsanch@gmail.com>
2019-10-18 19:09:26 +0200

    herokustatic files urls

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
