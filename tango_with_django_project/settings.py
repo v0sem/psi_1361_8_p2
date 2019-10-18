fe5b75a228c40cbf3abb58c6d885e46c306b8394
vosem <progpabsanch@gmail.com>
2019-10-18 19:08:09 +0200

    herokustatic files

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = STATIC_DIR + '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticHeroku')
 STATIC_URL = '/static/'
 
 LOGIN_URL = 'rango:login'
