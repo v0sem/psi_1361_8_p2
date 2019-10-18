46dbe6b5dc426fd2ad4f0403b19926840f7f9e4e
vosem <progpabsanch@gmail.com>
2019-10-18 18:55:36 +0200

    Lets see

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = STATIC_DIR
+STATIC_ROOT = STATIC_DIR + '/images/'
 STATIC_URL = '/static/'
 
 LOGIN_URL = 'rango:login'
